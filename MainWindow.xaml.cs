7bf40f6824134cb4cc60d188a9b4791fd0c4562a
eejopp <144217724+eejopp@users.noreply.github.com>
2024-02-16 18:17:27 +0300

    там too much файлов. все не помещаются. я не знаю что вам точно нужно
     все не помещаются. я не знаю что вам точно нужно чтобы все заработало

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PRACT2.Model;
+
+namespace PRACT2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        NoteManager noteManager=new NoteManager();
+        public MainWindow()
+        {
+            InitializeComponent();
+            noteManager=NoteManager.LoadFromFile("notes.json");            
+            listBox.ItemsSource = noteManager.DateNotes;
+            listBox.DisplayMemberPath = "Date";
+
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (listBox.SelectedItem == null) return;
+            DateNote dateNote= listBox.SelectedItem as DateNote;            
+            tbDescribe.Text = dateNote.Note.Description;
+            tbTitle.Text = dateNote.Note.Title;
+        }
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            DateNote dateNote = listBox.SelectedItem as DateNote;
+            dateNote.Note.Description = tbDescribe.Text;
+            dateNote.Note.Title = tbTitle.Text;
+            noteManager.SaveToFile("notes.json");
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem==null) return;
+            DateNote dateNote = listBox.SelectedItem as DateNote;
+            noteManager.RemoveNote(dateNote);
+            noteManager.SaveToFile("notes.json");
+            //Обновляем список на экране
+            listBox.ItemsSource = null;
+            listBox.ItemsSource = noteManager.DateNotes;
+            listBox.DisplayMemberPath = "Date";
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+            noteManager.AddNote(dpDate.DisplayDate, new Note() { Description = tbDescribe.Text, Title = tbTitle.Text });
+            noteManager.SaveToFile("notes.json");
+            //Обновляем список на экране
+            listBox.ItemsSource = null;
+            listBox.ItemsSource = noteManager.DateNotes;
+            listBox.DisplayMemberPath = "Date";
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace PRACT2.Model
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class DateNote
+    {
+        public DateTime Date { get; set; }
+        public Note Note { get; set; }
+
+        public DateNote(DateTime date, Note note)
+        {
+            Date = date;
+            Note = note;
+        }
+    }
+
+    public class NoteManager
+    {
+        public List<DateNote> DateNotes { get; set; }
+
+        public NoteManager()
+        {
+            DateNotes = new List<DateNote>();
+        }
+
+        public void AddNote(DateTime date, Note note)
+        {
+            DateNotes.Add(new DateNote(date, note));
+        }
+
+        public void UpdateNote(DateTime date, Note existingNote, Note updatedNote)
+        {
+            existingNote.Title = updatedNote.Title;
+            existingNote.Description = updatedNote.Description;
+
+        }
+
+        public void RemoveNote(DateNote dateNote)
+        {
+            DateNotes.Remove(dateNote);           
+        }
+
+        public void SaveToFile(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static NoteManager LoadFromFile(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<NoteManager>(json);
+        }
+    }
+}
